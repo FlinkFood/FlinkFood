d23fc2f34c60eb7290f0b8d4940df8dd58a56865
Luca Mozzoni <luca.mozzoni@studenti.unicam.it>
2023-11-10 08:19:36 +0000

    Added MongoDB sink

--- flinkfood-demo/src/main/java/org/flinkfood/flinkjobs/DataStreamJob.java
+++ flinkfood-demo/src/main/java/org/flinkfood/flinkjobs/DataStreamJob.java
@@ -1,65 +1,32 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.flinkfood.flinkjobs;
 
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.connector.mongodb.sink.MongoSink;
+import org.apache.flink.connector.base.DeliveryGuarantee;
+import com.mongodb.client.model.InsertOneModel;
+import org.bson.BsonDocument;
 
-/**
- * Skeleton for a Flink DataStream Job.
- *
- * <p>For a tutorial how to write a Flink application, check the
- * tutorials and examples on the <a href="https://flink.apache.org">Flink Website</a>.
- *
- * <p>To package your application into a JAR file for execution, run
- * 'mvn clean package' on the command line.
- *
- * <p>If you change the name of the main class (with the public static void main(String[] args))
- * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
- */
 public class DataStreamJob {
+        private static final String MONGODB_URI = System.getenv("MONGODB_URI");
+        private static final String DB_NAME = "flinkfood";
 
-	public static void main(String[] args) throws Exception {
-		// Sets up the execution environment, which is the main entry point
-		// to building Flink applications.
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        public static void main(String[] args) throws Exception {
+                MongoSink<String> sink = MongoSink.<String>builder()
+                                .setUri(MONGODB_URI)
+                                .setDatabase(DB_NAME)
+                                .setCollection("users_sink")
+                                .setBatchSize(1000)
+                                .setBatchIntervalMs(1000)
+                                .setMaxRetries(3)
+                                .setDeliveryGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
+                                .setSerializationSchema(
+                                                (input, context) -> new InsertOneModel<>(BsonDocument.parse(input)))
+                                .build();
 
-		/*
-		 * Here, you can start creating your execution plan for Flink.
-		 *
-		 * Start with getting some data from the environment, like
-		 * 	env.fromSequence(1, 10);
-		 *
-		 * then, transform the resulting DataStream<Long> using operations
-		 * like
-		 * 	.filter()
-		 * 	.flatMap()
-		 * 	.window()
-		 * 	.process()
-		 *
-		 * and many more.
-		 * Have a look at the programming guide:
-		 *
-		 * https://nightlies.apache.org/flink/flink-docs-stable/
-		 *
-		 */
+                Configuration conf = new Configuration();
+                StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(conf);
 
-		// Execute program, beginning computation.
-		env.execute("Flink Java API Skeleton");
-	}
+                env.execute("MongoDB to MongoBD table transfer");
+        }
 }
