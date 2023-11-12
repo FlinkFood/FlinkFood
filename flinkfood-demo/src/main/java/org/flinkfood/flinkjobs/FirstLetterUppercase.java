0962583ac8a8ac83101346b13255b6de657622d0
Luca Mozzoni <luca.mozzoni@studenti.unicam.it>
2023-11-12 18:40:28 +0000

    FLIN-86 updated Kafka topic name

--- flinkfood-demo/src/main/java/org/flinkfood/flinkjobs/FirstLetterUppercase.java
+++ flinkfood-demo/src/main/java/org/flinkfood/flinkjobs/FirstLetterUppercase.java
@@ -1,7 +1,5 @@
-// Package declaration for the Flink job
 package org.flinkfood.flinkjobs;
 
-// Importing necessary Flink libraries and external dependencies
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.MapFunction;
@@ -17,72 +15,61 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import com.mongodb.client.model.InsertOneModel;
 import org.bson.BsonDocument;
 
-// Class declaration for the Flink job
 public class FirstLetterUppercase {
-    
-    // Kafka and MongoDB connection details obtained from environment variables
-    private static final String KAFKA_URI = System.getenv("KAFKA_URI");
-    private static final String SOURCE_DB_TABLE = "dbserver1.inventory.customers";
-    private static final String MONGODB_URI = System.getenv("MONGODB_URI");
-    private static final String SINK_DB = "flinkfood";
-    private static final String SINK_DB_TABLE = "users_sink";
+        private static final String MONGODB_URI = System.getenv("MONGODB_URI");
+        private static final String KAFKA_URI = System.getenv("KAFKA_URI");
+        private static final String SOURCE_TOPIC = "postgres.public.client";
+        private static final String SINK_TABLE = "users_sink";
+        private static final String SINK_DB = "flinkfood";
 
-    // Main method where the Flink job is defined
-    public static void main(String[] args) throws Exception {
-        
-        // Setting up Kafka source with relevant configurations
-        KafkaSource<String> source = KafkaSource.<String>builder()
-                .setBootstrapServers(KAFKA_URI)
-                .setTopics(SOURCE_DB_TABLE)
-                .setGroupId("my-group")
-                .setStartingOffsets(OffsetsInitializer.earliest())
-                .setValueOnlyDeserializer(new SimpleStringSchema())
-                .build();
+        public static void main(String[] args) throws Exception {
 
-        // Setting up MongoDB sink with relevant configurations
-        MongoSink<String> sink = MongoSink.<String>builder()
-                .setUri(MONGODB_URI)
-                .setDatabase(SINK_DB)
-                .setCollection(SINK_DB_TABLE)
-                .setBatchSize(1000)
-                .setBatchIntervalMs(1000)
-                .setMaxRetries(3)
-                .setDeliveryGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
-                .setSerializationSchema(
-                        (input, context) -> new InsertOneModel<>(BsonDocument.parse(input)))
-                .build();
+                KafkaSource<String> source = KafkaSource.<String>builder()
+                                .setBootstrapServers(KAFKA_URI)
+                                .setTopics(SOURCE_TOPIC)
+                                .setGroupId("my-group")
+                                .setStartingOffsets(OffsetsInitializer.earliest())
+                                .setValueOnlyDeserializer(new SimpleStringSchema())
+                                .build();
 
-        // Setting up Flink execution environment
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+                MongoSink<String> sink = MongoSink.<String>builder()
+                                .setUri(MONGODB_URI)
+                                .setDatabase(SINK_DB)
+                                .setCollection(SINK_TABLE)
+                                .setBatchSize(1000)
+                                .setBatchIntervalMs(1000)
+                                .setMaxRetries(3)
+                                .setDeliveryGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
+                                .setSerializationSchema(
+                                                (input, context) -> new InsertOneModel<>(BsonDocument.parse(input)))
+                                .build();
 
-        // Creating a data stream from the Kafka source
-        DataStream<String> stream = env
-                .fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source")
-                .setParallelism(2);
+                StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        // Applying a map function to transform the data (capitalize the first letter of 'first_name' field)
-        stream
-                .map(new MapFunction<String, String>() {
-                    @Override
-                    public String map(String value) throws Exception {
-                        // Parsing JSON, modifying 'first_name', and converting back to a string
-                        ObjectMapper mapper = new ObjectMapper();
-                        JsonNode msg = mapper
-                                .readTree(value)
-                                .path("payload")
-                                .path("after");
+                DataStream<String> stream = env
+                                .fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source")
+                                .setParallelism(2);
 
-                        String name = msg.path("first_name").asText();
-                        name = name.substring(0, 1).toUpperCase() + name.substring(1);
-                        ((ObjectNode) msg).put("first_name", name);
+                stream
+                                .map(new MapFunction<String, String>() {
+                                        @Override
+                                        public String map(String value) throws Exception {
+                                                ObjectMapper mapper = new ObjectMapper();
+                                                JsonNode msg = mapper
+                                                                .readTree(value)
+                                                                .path("payload")
+                                                                .path("after");
 
-                        return mapper.writeValueAsString(msg);
-                    }
-                })
-                .setParallelism(1)
-                .sinkTo(sink);
+                                                String name = msg.path("name").asText();
+                                                name = name.substring(0, 1).toUpperCase() + name.substring(1);
+                                                ((ObjectNode) msg).put("name", name);
 
-        // Execute the Flink job with the given name
-        env.execute("FirstLetterUppercase");
-    }
+                                                return mapper.writeValueAsString(msg);
+                                        }
+                                })
+                                .setParallelism(1)
+                                .sinkTo(sink);
+
+                env.execute("FirstLetterUppercase");
+        }
 }
