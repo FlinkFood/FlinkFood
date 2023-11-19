8f7be9e811d179956e3de032d6485e3a7fad0dae
Vegard Sjåvik <vgsjaavik@gmail.com>
2023-11-19 15:44:55 +0100

    FLIN-89 Add topic for restaurant

--- startup.sh
+++ startup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 curl -X POST 'http://localhost:8083/connectors' -H 'Content-Type: application/json' -d '{
     "name": "postgres-users-connector",
     "config": {
@@ -10,6 +9,22 @@ curl -X POST 'http://localhost:8083/connectors' -H 'Content-Type: application/js
         "database.password": "postgres",
         "database.dbname": "flinkfood",
         "database.server.name": "postgres",
-        "table.include.list": "public.client"
+        "table.include.list": "public.client",
+        "slot.name": "users_replication_slot"
+    }
+}'
+
+curl -X POST 'http://localhost:8083/connectors' -H 'Content-Type: application/json' -d '{
+    "name": "postgres-restaurants-connector",
+    "config": {
+        "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+        "database.hostname": "postgres",
+        "database.port": "5432",
+        "database.user": "postgres",
+        "database.password": "postgres",
+        "database.dbname": "flinkfood",
+        "database.server.name": "postgres",
+        "table.include.list": "public.restaurant",
+        "slot.name": "restaurants_replication_slot"
     }
 }'
