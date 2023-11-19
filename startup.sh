eb346ca8b24ccb590f167b5008d70e2e70058c20
Vegard Sjåvik <vgsjaavik@gmail.com>
2023-11-19 16:50:59 +0100

    FLIN-89 Add topic creation for dishes

--- startup.sh
+++ startup.sh
@@ -28,3 +28,18 @@ curl -X POST 'http://localhost:8083/connectors' -H 'Content-Type: application/js
         "slot.name": "restaurants_replication_slot"
     }
 }'
+
+curl -X POST 'http://localhost:8083/connectors' -H 'Content-Type: application/json' -d '{
+    "name": "postgres-dishes-connector",
+    "config": {
+        "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+        "database.hostname": "postgres",
+        "database.port": "5432",
+        "database.user": "postgres",
+        "database.password": "postgres",
+        "database.dbname": "flinkfood",
+        "database.server.name": "postgres",
+        "table.include.list": "public.dishes",
+        "slot.name": "dishes_replication_slot"
+    }
+}'
