a03f1a87ffa382e221fe20e355ee252b797c5cba
Vegard Sjåvik <vgsjaavik@gmail.com>
2023-11-22 10:49:57 +0100

    FLIN-84 Load kafka topics for actual tables

--- flinkfood-demo/src/main/java/org/flinkfood/flinkjobs/ResturantView.java
+++ flinkfood-demo/src/main/java/org/flinkfood/flinkjobs/ResturantView.java
@@ -17,29 +17,101 @@ public class ResturantView {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
 
-        // Define the tables in TableEnvironment
-        tEnv.executeSql("CREATE TABLE restaurants (\n" +
-        " ID STRING,\n" +
-        " NAME STRING,\n" +
-        " CITY STRING,\n" +
-        " STATE STRING\n" +
+        // Define the restaurant_info table
+        tEnv.executeSql("CREATE TABLE restaurant_info (\n" +
+        " id BIGINT,\n" +
+        " name STRING,\n" +
+        " address STRING,\n" +
+        " phone STRING,\n" +
+        " email STRING,\n" +
+        " price_range STRING,\n" +
+        " cuisine_type STRING,\n" +
+        " vat_code INT\n" +
         ") WITH (\n" +
         " 'connector' = 'kafka',\n" +
-        " 'topic' = 'postgres.public.restaurant',\n" +
+        " 'topic' = 'postgres.public.restaurant_info',\n" +
         " 'properties.bootstrap.servers' = 'localhost:9092',\n" +
         " 'properties.group.id' = 'my-group',\n" +
         " 'format' = 'json'\n" +
         ")");
 
+        // Define the restaurant_services table
+        tEnv.executeSql("CREATE TABLE restaurant_services (\n" +
+        " restaurant_id INT,\n" +
+        " take_away BOOLEAN,\n" +
+        " delivery BOOLEAN,\n" +
+        " dine_in BOOLEAN,\n" +
+        " parking_lots INT,\n" +
+        " accessible BOOLEAN,\n" +
+        " children_area BOOLEAN,\n" +
+        " children_food BOOLEAN\n" +
+        ") WITH (\n" +
+        " 'connector' = 'kafka',\n" +
+        " 'topic' = 'postgres.public.restaurant_services',\n" +
+        " 'properties.bootstrap.servers' = 'localhost:9092',\n" +
+        " 'properties.group.id' = 'my-group',\n" +
+        " 'format' = 'json'\n" +
+        ")");
+
+        // Define the restaurant_address table
+        tEnv.executeSql("CREATE TABLE restaurant_address (\n" +
+        " restaurant_id INT,\n" +
+        " street STRING,\n" +
+        " address_number STRING,\n" +
+        " zip_code INT,\n" +
+        " city STRING,\n" +
+        " province STRING,\n" +
+        " country STRING\n" +
+        ") WITH (\n" +
+        " 'connector' = 'kafka',\n" +
+        " 'topic' = 'postgres.public.restaurant_address',\n" +
+        " 'properties.bootstrap.servers' = 'localhost:9092',\n" +
+        " 'properties.group.id' = 'my-group',\n" +
+        " 'format' = 'json'\n" +
+        ")");
+
+        // Define the restaurant_reviews table
+        tEnv.executeSql("CREATE TABLE restaurant_reviews (\n" +
+        " id INT,\n" +
+        " restaurant_id INT,\n" +
+        " customer_id INT,\n" +
+        " rating DECIMAL(3,1),\n" +
+        " comment STRING\n" +
+        ") WITH (\n" +
+        " 'connector' = 'kafka',\n" +
+        " 'topic' = 'postgres.public.restaurant_reviews',\n" +
+        " 'properties.bootstrap.servers' = 'localhost:9092',\n" +
+        " 'properties.group.id' = 'my-group',\n" +
+        " 'format' = 'json'\n" +
+        ")");
+
+        // Define the dishes table
         tEnv.executeSql("CREATE TABLE dishes (\n" +
-        " ID STRING,\n" +
-        " NAME STRING,\n" +
-        " PRICE DOUBLE,\n" +
-        " RATING INT,\n" +
-        " RESTAURANT_ID STRING\n" +
+        " id BIGINT,\n" +
+        " restaurant_id INT,\n" +
+        " name STRING,\n" +
+        " price DECIMAL(5,2),\n" +
+        " currency STRING,\n" +
+        " category STRING,\n" +
+        " description STRING\n" +
         ") WITH (\n" +
         " 'connector' = 'kafka',\n" +
         " 'topic' = 'postgres.public.dishes',\n" +
+        " 'properties.bootstrap.servers' = 'localhost:9092',\n" +
+        " 'properties.group.id' = 'my-group',\n" +
+        " 'format' = 'json'\n" +
+        ")");
+
+        // Define the review_dish table
+        tEnv.executeSql("CREATE TABLE review_dish (\n" +
+        " id INT,\n" +
+        " dish_id INT,\n" +
+        " customer_id INT,\n" +
+        " rating DECIMAL(3,1),\n" +
+        " comment STRING\n" +
+        ") WITH (\n" +
+        " 'connector' = 'kafka',\n" +
+        " 'topic' = 'postgres.public.review_dish',\n" +
         " 'properties.bootstrap.servers' = 'localhost:9092',\n" +
         " 'properties.group.id' = 'my-group',\n" +
         " 'format' = 'json'\n" +
