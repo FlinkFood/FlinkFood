c9ae0ecba6d762d682957fbdecc5a2688ae3a133
febagni <febagni@usp.br>
2023-11-24 15:00:11 +0100

    [FLIN-85] :sparkles: Add script to connect to all tables

--- startup.sh
+++ startup.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
-curl -X POST 'http://localhost:8083/connectors' -H 'Content-Type: application/json' -d '{
-    "name": "postgres-users-connector",
-    "config": {
-        "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
-        "database.hostname": "postgres",
-        "database.port": "5432",
-        "database.user": "postgres",
-        "database.password": "postgres",
-        "database.dbname": "flinkfood",
-        "database.server.name": "postgres",
-        "table.include.list": "public.client"
-    }
-}'
+KAFKA_CONNECT_URL='http://localhost:8083/connectors'
+
+# Set your database and Kafka configurations
+DATABASE_HOST="postgres"
+DATABASE_PORT="5432"
+DATABASE_USER="postgres"
+DATABASE_PASSWORD="postgres"
+DATABASE_NAME="flinkfood"
+KAFKA_SERVER_NAME="postgres"
+
+# Specify the list of tables to include
+TABLES=("public.restaurant_info" "public.restaurant_branch" "public.customer" "public.dishes" "public.dish_ingredients" "public.ingredients" "public.review_dish" "public.orders" "public.supplier" "public.items")
+
+# Loop through the tables and create connectors
+for TABLE in "${TABLES[@]}"; do
+    CONNECTOR_NAME="${TABLE//./-}-connector"
+
+    # Create connector configuration
+    CONNECTOR_CONFIG='{
+        "name": "'$CONNECTOR_NAME'",
+        "config": {
+            "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+            "database.hostname": "'$DATABASE_HOST'",
+            "database.port": "'$DATABASE_PORT'",
+            "database.user": "'$DATABASE_USER'",
+            "database.password": "'$DATABASE_PASSWORD'",
+            "database.dbname": "'$DATABASE_NAME'",
+            "database.server.name": "'$KAFKA_SERVER_NAME'",
+            "table.include.list": "'$TABLE'"
+        }
+    }'
+
+    # Post the connector configuration to Kafka Connect
+    curl -X POST "$KAFKA_CONNECT_URL" -H 'Content-Type: application/json' -d "$CONNECTOR_CONFIG"
+done
