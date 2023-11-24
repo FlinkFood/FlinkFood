a887918c58490f5a33c7bb834535e86b568513d9
Vegard Sjåvik <40039543+VegSja@users.noreply.github.com>
2023-11-24 11:49:06 +0100

    Feature/flin 84 kafka source resturants (#33)
    * FLIN-84 Add sql population script
    
    * FLIN-89 Typo fixes
    
    * FLIN-89 Add topic for restaurant
    
    * FLIN-89 Add sample ResturantView aggregation
    
    * FLIn-89 Add sample join procedure
    
    * FLIN-89 Add topic creation for dishes
    
    * FLIN-89 Add dependency to flink-tables
    
    * Add example table for dishes
    
    * FLIN-89 Add dependency to flink-json
    
    * FLIN-89 Add sample job for getting restaurant, joining and printing
    
    * FLIN-84 Update .gitignore
    
    * FLIN-84 Load kafka topics for actual tables
    
    * FLIN-84 Fix sql parsing errors
    
    * FLIN-84 Getting kafka stream without crashes
    
    * FLIN-84 Add replication for Debezium
    
    * FLIN-84 Add topics for restaurant
    
    * FLIN-84 Sample for restaurant_info processing
    
    * FLIN-84 Add replica full to all restaurant tables
    
    * FLIN-84 Add debezium deserialization for all topics
    
    * FLIN-84 Simple serializer to mongoDB
    
    * FLIN-84 Small fixes
    
    * FLIN-84 Boost slots and senders psql
    
    * FLIN-84 Cleaned up code
    
    * FLIN-84 Change name of SINK_DB_TABLE for restaurants
    
    * FLIN-84 Make customer sink work again...
    
    * Test
    
    * FLIN-84 Fix issue with sql join not returning every entry
    
    * FLIN-84 Updated documentation with how to run restaurantview
    
    * like this there are more chances that works :computer:
    
    * minor changes
    
    ---------
    
    Co-authored-by: Giovanni <10683631@polimi.it>

---
+++ flinkfood-demo/src/main/java/org/flinkfood/serializers/RowToBsonDocument.java
@@ -0,0 +1,22 @@
+package org.flinkfood.serializers;
+
+import com.mongodb.client.model.InsertOneModel;
+import com.mongodb.client.model.WriteModel;
+import org.apache.flink.connector.mongodb.sink.writer.context.MongoSinkContext;
+import org.apache.flink.connector.mongodb.sink.writer.serializer.MongoSerializationSchema;
+import org.apache.flink.types.Row;
+import org.bson.BsonDocument;
+import org.bson.BsonString;
+
+public class RowToBsonDocument implements MongoSerializationSchema<Row> {
+
+    @Override
+    public WriteModel<BsonDocument> serialize(Row row, MongoSinkContext mongoSinkContext) {
+        BsonDocument document = new BsonDocument();
+        String[] field_names = row.getFieldNames(true).stream().toArray(String[]::new);
+        for (int i = 0; i < row.getArity(); i++) {
+            document.append(field_names[i], new BsonString(String.valueOf(row.getField(i))));
+        }
+        return new InsertOneModel<>(document);
+    }
+}
