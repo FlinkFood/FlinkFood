db6320accd5db77efe8f551e74df68170d440d27
Luca Mozzoni <luca.mozzoni@studenti.unicam.it>
2023-11-26 23:38:35 +0000

    [FLIN-121] Documented Dish View job

--- flinkfood-demo/src/main/java/org/flinkfood/flinkjobs/DishViewJob.java
+++ flinkfood-demo/src/main/java/org/flinkfood/flinkjobs/DishViewJob.java
@@ -4,8 +4,6 @@ package org.flinkfood.flinkjobs;
 // Importing necessary Flink libraries and external dependencies
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.connector.mongodb.sink.MongoSink;
-import org.apache.flink.connector.base.DeliveryGuarantee;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -13,8 +11,6 @@ import org.apache.flink.table.api.*;
 import org.apache.flink.types.Row;
 import static org.apache.flink.table.api.Expressions.*;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
-import com.mongodb.client.model.InsertOneModel;
-import org.bson.BsonDocument;
 import org.flinkfood.schemas.*;
 
 // Class declaration for the Flink job
@@ -24,9 +20,6 @@ public class DishViewJob {
         private static final String KAFKA_URI = System.getenv("KAFKA_URI");
         private static final String SOURCE_DISH_TABLE = "postgres.public.dishes";
         private static final String SOURCE_RESTAURANT_INFO_TABLE = "postgres.public.restaurant_info";
-        private static final String MONGODB_URI = System.getenv("MONGODB_URI");
-        private static final String SINK_DB = "flinkfood";
-        private static final String SINK_DB_TABLE = "users_sink";
 
         // Main method where the Flink job is defined
         public static void main(String[] args) throws Exception {
@@ -49,53 +42,45 @@ public class DishViewJob {
                                 .setValueOnlyDeserializer(new KafkaRestaurantInfoSchema())
                                 .build();
 
-                // Setting up MongoDB sink with relevant configurations
-                MongoSink<Row> sink = MongoSink.<Row>builder()
-                                .setUri(MONGODB_URI)
-                                .setDatabase(SINK_DB)
-                                .setCollection(SINK_DB_TABLE)
-                                .setBatchSize(1000)
-                                .setBatchIntervalMs(1000)
-                                .setMaxRetries(3)
-                                .setDeliveryGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
-                                .setSerializationSchema(
-                                        // TODO: parse input and store it in MongoDB
-                                        // TODO: Determine if insert or update
-                                                (input, context) -> new InsertOneModel<>(BsonDocument.parse(input.toString())))
-                                .build();
-
                 // Setting up Flink execution environment
                 StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
                 StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
 
+                // Creates a Flink data stream for the dishes
                 DataStream<Dish> dishStream = env
-                                .fromSource(dishSource, WatermarkStrategy.noWatermarks(), "Kafka Dish Source")
-                                .setParallelism(1);
-
+                .fromSource(dishSource, WatermarkStrategy.noWatermarks(), "Kafka Dish Source")
+                .setParallelism(1);
+                
+                // Creates a Flink data stream for the restaurants
                 DataStream<RestaurantInfo> restaurantInfoStream = env
                                 .fromSource(restaurantInfoSource, WatermarkStrategy.noWatermarks(),
                                                 "Kafka Restaurant Info Source")
                                 .setParallelism(1);
 
+                // Creates a Flink table for the dishes
                 Table dishTable = tableEnv.fromDataStream(dishStream).select(
                                 $("id").as("dish_id"),
                                 $("name").as("dish_name"),
                                 $("restaurant_id"));
 
+                // Creates a Flink table for the restaurants
                 Table restaurantInfoTable = tableEnv.fromDataStream(restaurantInfoStream).select(
                                 $("id"),
                                 $("name").as("restaurant_name"));
 
+                // Joins the two tables in a single table and selects some arbitrary fields
                 Table result = dishTable
                                 .join(restaurantInfoTable)
                                 .where($("restaurant_id").isEqual($("id")))
                                 .select($("dish_name"), $("dish_id"), $("restaurant_name"));
 
+                // Converts the result table in a Flink data stream 
                 DataStream<Row> resultStream = tableEnv.toDataStream(result);
-
+                
+                // Print data coming from the stream in the console 
                 resultStream.print();
-                resultStream.sinkTo(sink);
-
-                env.execute("CustomerViewJob");
+                
+                // Starts job execution 
+                env.execute("DishViewJob");
         }
 }
