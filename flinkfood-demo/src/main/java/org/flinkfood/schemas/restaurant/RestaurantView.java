ef7ee8a16c023fb2df089394e4220c6ca89da5d7
Giovanni <10683631@polimi.it>
2023-12-01 18:04:06 +0100

    Updated serialisation

--- flinkfood-demo/src/main/java/org/flinkfood/schemas/restaurant/RestaurantView.java
+++ flinkfood-demo/src/main/java/org/flinkfood/schemas/restaurant/RestaurantView.java
@@ -6,11 +6,15 @@ import org.apache.flink.api.common.serialization.SerializationSchema;
 import org.apache.flink.connector.mongodb.sink.config.MongoWriteOptions;
 import org.apache.flink.connector.mongodb.sink.writer.context.MongoSinkContext;
 import org.apache.flink.connector.mongodb.sink.writer.serializer.MongoSerializationSchema;
+import org.apache.flink.mongodb.shaded.com.mongodb.client.model.UpdateOneModel;
+import org.apache.flink.mongodb.shaded.org.bson.BsonElement;
+import org.apache.flink.mongodb.shaded.org.bson.conversions.Bson;
 import org.bson.*;
 import org.flinkfood.schemas.dish.Dish;
 import org.flinkfood.schemas.order.Order;
 import org.flinkfood._helper.InsertBsonField;
 
+import java.util.Collections;
 import java.util.List;
 
 public class RestaurantView {
@@ -84,6 +88,42 @@ public class RestaurantView {
         return restaurantInfo.getId();
     }
 
+    public BsonDocument toBson() {
+        return new BsonDocument()
+                .append("restaurantId", new BsonInt32(restaurantInfo.getId()))
+                .append("restaurantName", new BsonString(restaurantInfo.getName()))
+                .append("restaurantAddress", new BsonDocument()
+                        .append("street", new BsonString(restaurantAddress.getStreet()))
+                        .append("city", new BsonString(restaurantAddress.getCity()))
+                        .append("state", new BsonString(restaurantAddress.getCountry()))
+                        //FIXME.append("zipCode", new BsonString(restaurantAddress.getZipCode()))
+                        .append("country", new BsonString(restaurantAddress.getCountry())))
+                //FIXME: lists must be lists
+                .append("restaurantService", new BsonDocument());
+                /*        .append("delivery", new BsonBoolean(restaurantService.isDelivery()))
+                        .append("takeout", new BsonBoolean(restaurantService.isTakeout()))
+                        .append("reserve", new BsonBoolean(restaurantService.isReserve())))
+                .append("restaurantReviews", new BsonArray(Collections.singletonList(
+                        new BsonDocument()
+                                .append("reviewId", new BsonInt32(restaurantReviews.get(0).getId()))
+                                .append("reviewerName", new BsonString(restaurantReviews.get(0).getReviewerName()))
+                                .append("reviewerEmail", new BsonString(restaurantReviews.get(0).getReviewerEmail()))
+                                .append("reviewerPhone", new BsonString(restaurantReviews.get(0).getReviewerPhone()))
+                                .append("reviewerRating", new BsonInt32(restaurantReviews.get(0).getReviewerRating()))
+                                .append("reviewerComment", new BsonString(restaurantReviews.get(0).getReviewerComment()))
+                                .append("reviewerDate", new BsonDateTime(restaurantReviews.get(0).getReviewerDate().getTime()))
+                )))
+                .append("dishes", new BsonArray(Collections.singletonList(
+                        new BsonDocument()
+                                .append("dishId", new BsonInt32(dishes.get(0).getId()))
+                                .append("dishName", new BsonString(dishes.get(0).getName()))
+                                .append("dishDescription", new BsonString(dishes.get(0).getDescription()))
+                                .append("dishPrice", new BsonDouble(dishes.get(0).getPrice()))
+                                .append("dishType", new BsonString(dishes.get(0).getType()))
+                                .append("*/
+
+    }
+
     public static class Serializer implements MongoSerializationSchema<RestaurantView>, InsertBsonField {
 
         @Override
@@ -94,7 +134,9 @@ public class RestaurantView {
 
         @Override
         public WriteModel<BsonDocument> serialize(RestaurantView element, MongoSinkContext sinkContext) {
-            return new InsertOneModel<>(new BsonDocument().append("restaurant_id", new BsonInt32(element.getRestaurantId())));
+            return new InsertOneModel<>(element.toBson());
+            // TODO: use this in case a Document of the view is already present
+            // return new UpdateOneModel<RestaurantView>(...)
         }
     }
 }
