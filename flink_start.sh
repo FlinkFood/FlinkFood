fbe353becfd2524e464574974bdc22a00c4fc934
Vegard Sjåvik <vgsjaavik@gmail.com>
2023-12-01 09:26:40 +0100

    FLIN-138 Some code cleanup

--- flink_start.sh
+++ flink_start.sh
@@ -7,6 +7,30 @@ export MONGODB_SERVER=mongodb://mongo:27017
 echo "Starting jobmanager..."
 /opt/flink/bin/jobmanager.sh start
 
+echo "Sending Kafka Connectors to Kafka Connect..."
+curl -X POST 'http://kconnect:8083/connectors' -H 'Content-Type: application/json' -d '{
+    "name": "postgres-connector",
+    "config": {
+        "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+        "database.hostname": "postgres",
+        "database.port": "5432",
+        "database.user": "postgres",
+        "database.password": "postgres",
+        "database.dbname": "flinkfood",
+        "database.server.name": "postgres",
+        "schema.whitelist": "public",
+        "transforms": "unwrap",
+        "transforms.unwrap.type": "io.debezium.transforms.ExtractNewRecordState",
+        "key.converter":"org.apache.kafka.connect.json.JsonConverter",
+        "key.converter.schemas.enable":false,
+        "value.converter":"org.apache.kafka.connect.json.JsonConverter",
+        "value.converter.schemas.enable":false,
+        "schemas.enable":false
+    }
+}' &&
+echo "\nWaiting 5 seconds for Kafka Connect to start...\n"
+sleep 5 && # Wait for Kafka Connect to start
+
 echo "Starting Flink jobs..."
 
 # Run Flink jobs
