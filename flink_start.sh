860a7f501490df9eec876877e3aee1ad9f6cb3cc
Vegard Sjåvik <vgsjaavik@gmail.com>
2023-12-04 15:24:28 +0100

    quickfix Add wait for kconnect before curling

--- flink_start.sh
+++ flink_start.sh
@@ -7,27 +7,33 @@ export MONGODB_SERVER=mongodb://mongo:27017
 echo "Starting jobmanager..."
 /opt/flink/bin/jobmanager.sh start
 
-sleep 10 &&
-echo "Sending Kafka Connectors to Kafka Connect..."
+# Wait until the Kafka Connect service is available
+while ! curl -s 'http://kconnect:8083/connectors' > /dev/null; do
+ echo "Waiting for Kafka Connect to be available..."
+ sleep 5
+done
+
+echo "Kafka Connect is now available. Sending Kafka Connectors to Kafka Connect..."
+
 curl -X POST 'http://kconnect:8083/connectors' -H 'Content-Type: application/json' -d '{
-    "name": "postgres-connector",
-    "config": {
-        "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
-        "database.hostname": "postgres",
-        "database.port": "5432",
-        "database.user": "postgres",
-        "database.password": "postgres",
-        "database.dbname": "flinkfood",
-        "database.server.name": "postgres",
-        "schema.whitelist": "public",
-        "transforms": "unwrap",
-        "transforms.unwrap.type": "io.debezium.transforms.ExtractNewRecordState",
-        "key.converter":"org.apache.kafka.connect.json.JsonConverter",
-        "key.converter.schemas.enable":false,
-        "value.converter":"org.apache.kafka.connect.json.JsonConverter",
-        "value.converter.schemas.enable":false,
-        "schemas.enable":false
-    }
+   "name": "postgres-connector",
+   "config": {
+       "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+       "database.hostname": "postgres",
+       "database.port": "5432",
+       "database.user": "postgres",
+       "database.password": "postgres",
+       "database.dbname": "flinkfood",
+       "database.server.name": "postgres",
+       "schema.whitelist": "public",
+       "transforms": "unwrap",
+       "transforms.unwrap.type": "io.debezium.transforms.ExtractNewRecordState",
+       "key.converter":"org.apache.kafka.connect.json.JsonConverter",
+       "key.converter.schemas.enable":false,
+       "value.converter":"org.apache.kafka.connect.json.JsonConverter",
+       "value.converter.schemas.enable":false,
+       "schemas.enable":false
+   }
 }' &&
 echo "\nWaiting 5 seconds for Kafka Connect to start...\n"
 sleep 5 && # Wait for Kafka Connect to start
@@ -35,9 +41,9 @@ sleep 5 && # Wait for Kafka Connect to start
 echo "Starting Flink jobs..."
 
 # Run Flink jobs
-flink run --detached /opt/flink/CustomerViewJob/target/customerview-1.0.jar &&
-flink run --detached /opt/flink/RestaurantViewJob/target/restaurantview-1.0.jar &&
-flink run --detached /opt/flink/DishViewJob/target/dishview-1.0.jar &&
+/opt/flink/bin/flink run --detached /opt/flink/CustomerViewJob/target/customerview-1.0.jar &&
+/opt/flink/bin/flink run --detached /opt/flink/RestaurantViewJob/target/restaurantview-1.0.jar &&
+/opt/flink/bin/flink run --detached /opt/flink/DishViewJob/target/dishview-1.0.jar &&
 
 echo "Flink jobs started."
 
