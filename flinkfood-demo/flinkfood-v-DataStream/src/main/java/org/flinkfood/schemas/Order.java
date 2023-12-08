b77660a697075d1adac93b10c01131ca03f610f8
Giovanni <10683631@polimi.it>
2023-12-08 11:30:27 +0100

    refactoring -> new project structure

---
+++ flinkfood-demo/flinkfood-v-DataStream/src/main/java/org/flinkfood/schemas/Order.java
@@ -0,0 +1,72 @@
+package org.flinkfood.schemas;
+
+public class Order {
+
+    public int id;
+    public int customer_id;
+    public int total_amount;
+    public String order_id;
+    public String title;
+    public String restaurant;
+    public boolean paid;
+
+    public Order() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public int getTotal_amount() {
+        return total_amount;
+    }
+
+    public void setTotal_amount(int total_amount) {
+        this.total_amount = total_amount;
+    }
+
+    public String getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(String order_id) {
+        this.order_id = order_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(String restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public boolean isPaid() {
+        return paid;
+    }
+
+    public void setPaid(boolean paid) {
+        this.paid = paid;
+    }
+
+}
