4fdf4da5846028315234e70a61adc0d900aa9d7f
Marcos Pietrucci <marcospietrucci@gmail.com>
2023-12-18 16:54:37 +0100

    FlinkTable created + yaml file updated

---
+++ flinkfood-demo/RestaurantViewJob/src/main/java/org/flinkfood/config_reader/FlinkTable.java
@@ -0,0 +1,14 @@
+package org.flinkfood.config_reader;
+
+
+public class FlinkTable {
+
+    private String name;
+    private String schema;
+    private String kafka_topic;
+
+    FlinkTable(){
+
+    }
+}
+
