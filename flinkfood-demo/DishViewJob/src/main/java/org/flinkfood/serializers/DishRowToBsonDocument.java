dd67ae5d18ba7429215383ecf75652b8c7bf0dd6
Luca Mozzoni <luca.mozzoni@studenti.unicam.it>
2023-12-18 18:54:05 +0000

    Added null check to dish SV serializer

--- flinkfood-demo/DishViewJob/src/main/java/org/flinkfood/serializers/DishRowToBsonDocument.java
+++ flinkfood-demo/DishViewJob/src/main/java/org/flinkfood/serializers/DishRowToBsonDocument.java
@@ -6,6 +6,7 @@ import com.mongodb.client.model.ReplaceOptions;
 import com.mongodb.client.model.WriteModel;
 import org.apache.flink.connector.mongodb.sink.writer.context.MongoSinkContext;
 import org.apache.flink.connector.mongodb.sink.writer.serializer.MongoSerializationSchema;
+import org.apache.flink.mongodb.shaded.org.bson.BsonNull;
 import org.apache.flink.types.Row;
 import org.bson.BsonArray;
 import org.bson.BsonDocument;
@@ -22,7 +23,10 @@ public class DishRowToBsonDocument implements MongoSerializationSchema<Row> {
         BsonArray ingredientsArray = new BsonArray();
         document.getArray("ingredients").forEach((ingredient) -> {
             BsonDocument ingredientDocument = BsonDocument.parse(ingredient.asString().getValue());
-            ingredientsArray.add(ingredientDocument);
+            if (!ingredientDocument.get("ingredient_id").isNull()){
+                System.out.println(ingredientDocument.get("ingredient_id"));
+                ingredientsArray.add(ingredientDocument);
+            }
         }); 
         document.put("ingredients", ingredientsArray);
         
