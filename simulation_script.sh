6c1db3992f83e992089e4136e66766a21684a0a0
Vegard Sjåvik <vgsjaavik@gmail.com>
2023-12-27 18:59:07 +0100

    FLIN-177 Simulate restaurant update

--- simulation_script.sh
+++ simulation_script.sh
@@ -9,8 +9,32 @@ DB_PASSWORD="postgres"
 
 export PGPASSWORD="$DB_PASSWORD"
 
-insert_order() {
+update_restaurant_info() {
+    # Generate random data variations
+    restaurant_id=$(shuf -i 1-9 -n 1)
+    phone_suffix=$(shuf -i 1000000000-9999999999 -n 1)
+    email_suffix=$(shuf -i 1-100 -n 1)
+    cuisine_type_suffix=$(shuf -i 1-5 -n 1)
+    price_range_suffix=$(shuf -e low medium high -n 1)
+    vat_code_suffix=$(shuf -i 100000000-999999999 -n 1)
 
+    # Construct the SQL query to update specific columns in restaurant_info
+    sql_query="UPDATE public.restaurant_info SET
+                phone = '$phone_suffix',
+                email = 'new.email$email_suffix@example.com',
+                cuisine_type = '$cuisine_type_suffix',
+                price_range = '$price_range_suffix',
+                vat_code = $vat_code_suffix
+                WHERE id = $restaurant_id;"
+
+    # Execute the SQL query
+    psql -h $DB_HOST -p $DB_PORT -d $DB_NAME -U $DB_USER -c "$sql_query"
+
+    echo "Updated specific columns in restaurant_info for restaurant_id $restaurant_id."
+    sleep 10
+}
+
+insert_order() {
     # Generate random data
     name="Order_$(shuf -i 1-10000000 -n 1)"
     customer_id=$(shuf -i 1-5 -n 1)
@@ -30,10 +54,11 @@ insert_order() {
     psql -h $DB_HOST -p $DB_PORT -d $DB_NAME -U $DB_USER -c "$sql_query"
 
     echo "Inserted a $name into the database."
+    sleep 5
 }
 
 while true; do
     # Sleep for 5 seconds before the next iteration
     insert_order
-    sleep 5
+    update_restaurant_info
 done
