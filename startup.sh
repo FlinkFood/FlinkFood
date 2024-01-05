acd221eae773a0e42ab80aafb10b0e4630bc63dd
Luca Mozzoni <luca.mozzoni@studenti.unicam.it>
2024-01-05 18:10:04 +0100

    Edit to startup.sh

--- startup.sh
+++ startup.sh
@@ -9,6 +9,8 @@ curl -X POST 'http://localhost:8083/connectors' -H 'Content-Type: application/js
         "database.dbname": "flinkfood",
         "database.server.name": "postgres",
         "schema.whitelist": "public",
+        "topic.creation.default.replication.factor":2,
+        "topic.creation.default.partitions":2,
         "transforms.unwrap.type": "io.debezium.transforms.ExtractNewRecordState",
         "key.converter":"org.apache.kafka.connect.json.JsonConverter",
         "key.converter.schemas.enable":false,
